ff29c2e2b6276ffb225f222683c3c25c7aa6cbce
Bayrakdar <bayrakdar@Bayrakdar-MacBook-Air.local>
2019-01-05 04:36:32 +0300

    Zar image

--- convert_run.sh
+++ convert_run.sh
@@ -1,4 +1,3 @@
 python image_to_text.py zar.jpg zar.txt
 python make_noise.py zar.txt 0.15 zar_noisy.txt
-python text_to_image.py zar.txt output.jpg
 python text_to_image.py zar_noisy.txt output.jpg
