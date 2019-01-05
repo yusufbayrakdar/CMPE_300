478935ad7608d3cc28dacd067067da14723ca9d7
Bayrakdar <bayrakdar@Bayrakdar-MacBook-Air.local>
2019-01-05 13:24:35 +0300

    düzeltildi

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-mpic++ -o  square square.cc
+mpic++ -o  vertical_only vertical_only.cc
 echo Successfully compiled!
-mpiexec -n  5 ./square zar_noisy.txt  output.txt 0.4 0.15
+mpiexec -n  5 ./vertical_only yinyang_noisy.txt  output.txt 0.4 0.15
 echo Done
 python text_to_image.py output.txt output.jpg
